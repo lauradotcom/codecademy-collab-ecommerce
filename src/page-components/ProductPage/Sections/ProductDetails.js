7bb95e16ef14d714ab33424a0aba55f01595f54e
Chris DeCleene <chris_decleene@ymail.com>
2022-01-04 14:06:13 -0700

    Add product to props and render product info using product.

--- src/page-components/ProductPage/Sections/ProductDetails.js
+++ src/page-components/ProductPage/Sections/ProductDetails.js
@@ -2,14 +2,15 @@ import * as React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 import { root } from './ProductDetails.module.css';
 
-const ProductDetails = () => {
+
+const ProductDetails = ({ product }) => {
+
   return (
     <div className={root}>
-      <h2>Product Name</h2>
-      <h2>$38.00</h2>
+      <h2>{product.name}</h2>
+      <h2>${product.price}.00</h2>
       <p>
-        Product description with lots of details wow here are all the features
-        you need to know about and then you will buy! Buy it now!
+        {product.description}
       </p>
     </div>
   );
