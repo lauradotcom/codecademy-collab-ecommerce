cd7416c0ca67e6f27e00d86b1efd7208d93c2269
Chris DeCleene <chris_decleene@ymail.com>
2022-01-04 18:08:37 -0700

    Import product data and pass first item into Cart Actions and Product Details components.

--- src/page-components/ProductPage/index.js
+++ src/page-components/ProductPage/index.js
@@ -1,23 +1,25 @@
-import * as React from 'react';
+import * as React from "react";
 // components
 // page sections
-import ProductImage from './Sections/ProductImage';
-import ProductDetails from './Sections/ProductDetails';
-import CartActions from './Sections/CartActions';
+import ProductImage from "./Sections/ProductImage";
+import ProductDetails from "./Sections/ProductDetails";
+import CartActions from "./Sections/CartActions";
 // styles
-import { main, rightColumn } from './ProductPage.module.css';
-// asset import
-import image1 from '../../images/product1-1.png';
+import { main, rightColumn } from "./ProductPage.module.css";
+
+// pull dummy item from data file
+import { products } from "../../data/products";
 
 const ProductPage = () => {
+  const product = products[0];
   return (
     <>
       <title>Product Page</title>
       <main className={main}>
-        <ProductImage image={image1} />
+        <ProductImage image={product.images[0]} />
         <div className={rightColumn}>
-          <ProductDetails />
-          <CartActions />
+          <ProductDetails product={product} />
+          <CartActions product={product} />
         </div>
       </main>
     </>
