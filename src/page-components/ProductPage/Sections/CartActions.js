3d62f3c6bb045565488b4861663f5627dbdb5c28
Chris DeCleene <chris_decleene@ymail.com>
2022-01-04 18:38:15 -0700

    Connected Redux cart. Added function to addItem to store on submit.

--- src/page-components/ProductPage/Sections/CartActions.js
+++ src/page-components/ProductPage/Sections/CartActions.js
@@ -1,11 +1,23 @@
-import * as React from 'react';
+import * as React from "react";
+import { useDispatch } from "react-redux";
 // components
-import Button from '../../../components/Buttons/MainButton';
+import Button from "../../../components/Buttons/MainButton";
+import { addItem } from "../../../state/cartSlice";
 // styles
-import { root, quantityButton, quantityText } from './CartActions.module.css';
+import { root, quantityButton, quantityText } from "./CartActions.module.css";
 
-const CartActions = () => {
-  const [quantity, setQuantity] = React.useState(0);
+const CartActions = ({ product }) => {
+  const dispatch = useDispatch();
+  const [quantity, setQuantity] = React.useState(1);
+
+  const addItemToCart = () => {
+    dispatch(
+      addItem({
+        ...product,
+        quantity: quantity,
+      })
+    );
+  };
 
   return (
     <div className={root}>
@@ -13,6 +25,7 @@ const CartActions = () => {
         <button
           className={quantityButton}
           onClick={() => setQuantity(quantity - 1)}
+          disabled={quantity <= 1}
         >
           -
         </button>
@@ -20,15 +33,14 @@ const CartActions = () => {
         <button
           className={quantityButton}
           onClick={() => setQuantity(quantity + 1)}
+          disabled={quantity <= 30}
         >
           +
         </button>
       </div>
       <Button
-        onClick={() =>
-          alert(`Thanks for testing the cart!\nQuantity of items: ${quantity}`)
-        }
-        buttonText='Add to cart'
+        onClick={addItemToCart}
+        buttonText="Add to cart"
       />
       <div>
         <span>Free shipping on orders over $50*</span>
