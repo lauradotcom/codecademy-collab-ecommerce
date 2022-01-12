b2a7b78f862f919d8097a631929ac7f78b289003
CxrlosKenobi <kenobi@ieee.org>
2022-01-12 01:28:24 -0300

    Update buttons inside CartDrawer

--- src/components/Layout/CartDrawer/index.js
+++ src/components/Layout/CartDrawer/index.js
@@ -60,7 +60,7 @@ const CartDrawer = ({ closeCart }) => {
                     <button
                       className={styles.quantityButton}
                       onClick={() => updateItemQuantity(product, false)}
-                      className={style.quantityButton}
+                      className={styles.quantityButton}
                     >
                       -
                     </button>
