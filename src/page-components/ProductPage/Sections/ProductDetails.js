f0daa737590720427a0b18018bee7c67287e196d
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2022-01-11 21:43:11 -0800

    Refactor ProductDetails to use CSS Modules

--- src/page-components/ProductPage/Sections/ProductDetails.js
+++ src/page-components/ProductPage/Sections/ProductDetails.js
@@ -1,42 +1,20 @@
 import * as React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
-import { root } from './ProductDetails.module.css';
+import * as styles from './ProductDetails.module.css';
 
-// I add the styles here because using the external css file didn't work idk why.
-// I'm open to suggestions on how to do this better
-const productTitle = {
-  fontSize: '1.5rem',
-  fontWeight: 'bold',
-  marginTop: '7%'
-}
-const pricesContainer = {
-  display: 'flex',
-  justifyContent: 'space-evenly',
-  alignItems: 'center',
-  marginLeft: '35%',
-  marginRight: '35%',
-}
-const priceStyles = {
-  fontWeight: 'bold'
-}
-const discountPriceStyles = {
-  color: 'gray',
-  textDecoration: 'line-through',
-  fontSize: '1.3rem'
-}
 
 const ProductDetails = ({ product }) => {
 
   return (
-    <div className={root}>
-      <h2 style={productTitle}>
+    <div className={styles.root}>
+      <h2 className={styles.productTitle}>
         {product.name}
       </h2>
-      <div style={pricesContainer}>
-        <h2 style={priceStyles}>
+      <div style={styles.pricesContainer}>
+        <h2 style={styles.price}>
           ${(product.price * product.discount).toFixed(2)}
         </h2>
-        <h2 style={discountPriceStyles}>
+        <h2 style={styles.discountPrice}>
           ${product.price.toFixed(2)}
         </h2>
       </div>
