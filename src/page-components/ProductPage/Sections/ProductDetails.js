a26a92f6724ee3a1dec89506d2141b1a0fc6f7af
Chris DeCleene <chris_decleene@ymail.com>
2022-01-13 14:07:16 -0700

    Conditionally render a discount price if there is a discount tied to the item else show regular price only.

--- src/page-components/ProductPage/Sections/ProductDetails.js
+++ src/page-components/ProductPage/Sections/ProductDetails.js
@@ -6,16 +6,16 @@ import * as styles from "./ProductDetails.module.css";
 const ProductDetails = ({ product }) => {
   return (
     <div className={styles.root}>
-      <h2 className={styles.productTitle}>
-        {product.name}
-      </h2>
+      <h2 className={styles.productTitle}>{product.name}</h2>
       <div className={styles.pricesContainer}>
         <h2 className={styles.price}>
-          ${(product.price * product.discount).toFixed(2)}
+          {product.discount > 0
+            ? formatPrice(product.price * product.discount)
+            : formatPrice(product.price)}
         </h2>
-        <h2 className={styles.discountPrice}>
-          ${product.price.toFixed(2)}
-        </h2>
+        {product.discount > 0 && (
+          <h2 className={styles.discountPrice}>${product.price.toFixed(2)}</h2>
+        )}
       </div>
       <p>{product.description}</p>
     </div>
