205b5cc82449a1aba42779e5578c4b188bf6c4ff
Chris DeCleene <chris_decleene@ymail.com>
2022-01-14 14:34:08 -0700

    Set up graphql query for all products

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,8 +1,30 @@
 import * as React from "react";
+import { graphql } from "gatsby";
 import ProductPage from "../../page-components/ProductPage";
 
-const Product = () => {
-  return <ProductPage />;
+const Product = ({ data }) => {
+  const products = data.allProductsJson.nodes;
+  return <ProductPage products={products} />;
 };
+
+export const query = graphql`
+  {
+    allProductsJson {
+      nodes {
+        description
+        discount
+        name
+        price
+        productId
+        id
+        images {
+          childImageSharp {
+            gatsbyImageData(layout: FIXED, placeholder: TRACED_SVG)
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Product;
