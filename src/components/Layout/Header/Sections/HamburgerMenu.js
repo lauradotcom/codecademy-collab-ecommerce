05ded68a629649d7852c0fa30cd8a3fb164a0feb
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2022-01-13 17:14:05 -0800

    Move menu links from Header to HamburgerMenu component

--- src/components/Layout/Header/Sections/HamburgerMenu.js
+++ src/components/Layout/Header/Sections/HamburgerMenu.js
@@ -1,13 +1,27 @@
 import React from "react";
-
+import { Link } from "gatsby";
 import { FaBars } from "react-icons/fa";
 
 import * as styles from "./HamburgerMenu.module.css";
 
 const HamburgerMenu = () => {
 
+  const listItems = [
+    { link: '/product', name: 'Product' },
+    { link: '/test', name: 'Test' },
+  ];
+
   return (
-    <FaBars />
+    <>
+      <FaBars />
+      <ul className={styles.headerMenu}>
+    {listItems.map((item) => (
+        <li>
+        <Link to={item.link}>{item.name}</Link>
+        </li>
+    ))}
+      </ul>
+    </>
   );
 };
 
--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -8,11 +8,6 @@ import * as styles from './index.module.css';
 
 const Header = ({ toggleCart }) => {
 
-  const listItems = [
-    { link: '/product', name: 'Product' },
-    { link: '/test', name: 'Test' },
-  ];
-
   return (
     <header className={styles.header}>
       <HamburgerMenu />
@@ -20,13 +15,6 @@ const Header = ({ toggleCart }) => {
         <p>EcommStore</p>
       </Link>
       <CartButton toggleCart={toggleCart} />
-      <ul className={styles.headerMenu}>
-        {listItems.map((item) => (
-          <li>
-            <Link to={item.link}>{item.name}</Link>
-          </li>
-        ))}
-      </ul>
     </header>
   );
 };
