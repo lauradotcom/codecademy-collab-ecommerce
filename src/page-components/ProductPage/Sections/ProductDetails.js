d6337308c570ce91741c7183e668bf3c07c08b6a
Chris DeCleene <chris_decleene@ymail.com>
2022-01-14 14:38:14 -0700

    Render ProductPrice component and move pricing styling out of module.

--- src/page-components/ProductPage/Sections/ProductDetails.js
+++ src/page-components/ProductPage/Sections/ProductDetails.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as styles from "./ProductDetails.module.css";
 import ProductPrice from "./ProductPrice";
-j
+
 const ProductDetails = ({ product }) => {
   return (
     <div className={styles.root}>
