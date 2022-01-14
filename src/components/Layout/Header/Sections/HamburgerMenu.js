bd3783faaadfa106bc87f63ec757cf35bd859d2a
Chris DeCleene <chris_decleene@ymail.com>
2022-01-14 10:09:17 -0700

    Improve accessibility a11y of hamburger menu

--- src/components/Layout/Header/Sections/HamburgerMenu.js
+++ src/components/Layout/Header/Sections/HamburgerMenu.js
@@ -5,10 +5,13 @@ import * as styles from "./HamburgerMenu.module.css";
 const HamburgerMenu = ({ showMenu, toggleMenu }) => {
 
   return (
-    <div className={styles.root} role="button">
+    <div className={styles.root}>
       <div
-        className={showMenu ? `${styles.active}` : ''}
+        className={showMenu ? `${styles.active}` : ""}
         onClick={toggleMenu}
+        onKeyPress={toggleMenu}
+        tabIndex={0}
+        role="button"
       >
         <span className={styles.bar}></span>
         <span className={styles.bar}></span>
