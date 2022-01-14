b83ebb5fe213afff4825e3f8ea9048e9598c6a82
Chris DeCleene <chris_decleene@ymail.com>
2022-01-14 09:48:35 -0700

    Remove unused redux state

--- src/components/Layout/Header/Sections/CartButton.js
+++ src/components/Layout/Header/Sections/CartButton.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { cartSelector } from "../../../../state/cartSlice";
 //
 import { FaShoppingBag } from "react-icons/fa";
 import CartQuantity from "./CartQuantity";
 import * as styles from "./CartButton.module.css";
 
 const CartButton = ({ toggleCart }) => {
-  // useSelector accesses Redux state by passing in the cartSelector
-  const cart = useSelector(cartSelector);
 
   return (
     <div className={styles.cartButtonContainer}>
