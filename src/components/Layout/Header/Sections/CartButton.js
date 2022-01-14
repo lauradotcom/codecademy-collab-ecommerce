c5891d7dd1aed82f06c59aaafe25278f9a3283fb
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2022-01-13 16:04:42 -0800

    Initialize CartButton and HamburgerMenu components and stylesheets

---
+++ src/components/Layout/Header/Sections/CartButton.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+import * as styles from "./CartButton.module.css";
+
+export default CartButton;
---
+++ src/components/Layout/Header/Sections/HamburgerMenu.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+import * as styles from "./HamburgerMenu.module.css";
+
+export default HamburgerMenu;
