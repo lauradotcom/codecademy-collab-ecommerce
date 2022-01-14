1f7d6e71e451acc6b3f784b6529dd22f4da9e0b9
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2022-01-13 17:01:54 -0800

    Import FA menu icon and create HamburgerMenu function that renders it

--- src/components/Layout/Header/Sections/HamburgerMenu.js
+++ src/components/Layout/Header/Sections/HamburgerMenu.js
@@ -1,5 +1,14 @@
 import React from "react";
 
+import { FaBars } from "react-icons/fa";
+
 import * as styles from "./HamburgerMenu.module.css";
+
+const HamburgerMenu = () => {
+
+  return (
+    <FaBars />
+  );
+};
 
 export default HamburgerMenu;
