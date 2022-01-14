2ba613b9dcd59c34184464e7d2a93b47626e6132
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2022-01-13 18:23:42 -0800

    Move menu links and related logic from HamburgerMenu to MenuModal

--- src/components/Layout/Header/Sections/HamburgerMenu.js
+++ src/components/Layout/Header/Sections/HamburgerMenu.js
@@ -6,21 +6,9 @@ import * as styles from "./HamburgerMenu.module.css";
 
 const HamburgerMenu = ({ open, setOpen }) => {
 
-  const listItems = [
-    { link: '/product', name: 'Product' },
-    { link: '/test', name: 'Test' },
-  ];
-
   return (
     <>
       <FaBars open={open} onClick={() => setOpen(!open)} />
-      <ul className={styles.headerMenu}>
-    {listItems.map((item) => (
-        <li>
-        <Link to={item.link}>{item.name}</Link>
-        </li>
-    ))}
-      </ul>
     </>
   );
 };
--- src/components/Layout/Header/Sections/MenuModal.js
+++ src/components/Layout/Header/Sections/MenuModal.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "gatsby";
 
 import * as styles from "./MenuModal.module.css";
 
 const MenuModal = () => {
-  
+
+  const listItems = [
+    { link: '/product', name: 'Product' },
+    { link: '/test', name: 'Test' },
+  ];
+
+  return (
+    <ul className={styles.headerMenu}>
+    {listItems.map((item) => (
+        <li>
+        <Link to={item.link}>{item.name}</Link>
+        </li>
+    ))}
+      </ul>
+  )
 }
 
 export default MenuModal;
