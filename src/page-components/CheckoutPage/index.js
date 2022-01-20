0814ab6beb3011299477a59d8f644ecb310e1b92
CxrlosKenobi <kenobi@ieee.org>
2022-01-20 15:24:51 -0300

    Update Final views for Shipping & Checkout

--- src/page-components/CheckoutPage/index.js
+++ src/page-components/CheckoutPage/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { cartSelector } from "../../state/cartSlice";
 //
@@ -7,12 +7,36 @@ import EmptyCartMessage from "../../components/Layout/CartDrawer/Sections/EmptyC
 //
 import * as styles from "./index.module.css";
 
+
 const CheckoutPage = () => {
   const cart = useSelector(cartSelector);
+  const [page, setPage] = useState('Information');
 
-  return (
-    <main className={styles.main}>
-      <section className={styles.sectionOne}>
+  const Breadcrumb = () => {
+    return (
+      <div className={styles.breadcrumb}>
+        <span 
+          id={styles.info} 
+          onClick={() => {
+            setPage('Information');
+          }}>
+          Information
+        </span>
+        <span> > </span>
+        <span 
+          id={styles.shipp} 
+          onClick={() => {
+            setPage('Shipping');
+          }}>
+          Shipping
+        </span>
+      </div>
+    )
+  }
+
+  const Form = () => {
+    return (
+      <>
         <h1>Checkout</h1>
         <div className={styles.form}>
           <h3>Contact information</h3>
@@ -35,32 +59,90 @@ const CheckoutPage = () => {
 
         </div>
         <div className={styles.buttonsRow}>
-          <button className={styles.shippingButton}>
+          <button className={styles.shippingButton} onClick={() => {setPage('Shipping')}}>
             Continue to shipping
           </button>
-          <button 
-            className={styles.backButton}
-            onClick={() => {
-              window.history.back();
-            }}
-          >
+          <button className={styles.backButton} onClick={() => {window.history.back()}}>
             Back
           </button>
         </div>
+      </>
+    );
+  }
+  
+  const Shipping = () => {
+    return (
+      <>
+        <h3>Shiping method</h3>
+        <div className={styles.shippMethod}>
+          <div className={styles.shippRow}>
+            <input type='radio' name='usps'/>
+            <div className={styles.childrens}>
+              <div>
+                <label>USPS Priority Mail</label>
+                <p>5 business days</p>
+              </div>
+              <span>$4.90</span>
+            </div>
+          </div>
+          <div className={styles.rowDivider}/>
+          <div className={styles.shippRow}>
+            <input type='radio' name='usps-express'/>
+            <div className={styles.childrens}>
+              <div>
+                <label>USPS Priority Mail Express</label>
+                <p>1 business days</p>
+              </div>
+              <span>$14.90</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.buttonsRow}>
+          <button className={styles.shippingButton}>
+            Checkout order
+          </button>
+          <button className={styles.backButton} onClick={() => {setPage('Information')}}>
+            Back
+          </button>
+        </div>
+      </>
+    );
+  }
+  
+  
+  const [content, setContent] = useState(<Form />);
+  useEffect(() => {
+    if (page === 'Information') {
+      document.title = 'Checkout';
+      setContent(<Form />);
+    } else if (page === 'Shipping') {
+      document.title = 'Shipping';
+      setContent(<Shipping />);
+    } else {
+      document.title = 'Checkout';
+    }
+  }, [page])
+
+  return (
+    <main className={styles.main}>
+      <section className={styles.sectionOne}>
+        <Breadcrumb />
+        {content}
       </section>
       <div className={styles.separator}/>
       <section className={styles.sectionTwo}>
+        <h1>Order summary</h1>
         {cart.totalQuantity > 0 ? (
           <>
             <CartProducts products={cart.items} />
             <div className={styles.hSeparator} />
-            <div className={styles.subtotalContainer}> {/* */}
+            <div className={styles.subtotalContainer}>
             
-              <div className={styles.subtotal}>
+              <div>
                 <span className={styles.subL}>Subtotal:</span>
                 <span className={styles.subR}>${cart.totalPrice.toFixed(2)}</span>
               </div>
-              <div className={styles.shipping}>
+              <div>
                 <span className={styles.subL}>Shipping:</span>
                 <span className={styles.subR}>Calculated at next step</span>
               </div>
