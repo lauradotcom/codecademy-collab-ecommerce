14192f01f9ef3d40e94c7263d4122fa61b777319
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2022-01-21 15:05:58 -0800

    Format CartHeader with Grid and style the header title

--- src/components/Layout/CartDrawer/Sections/CartHeader.js
+++ src/components/Layout/CartDrawer/Sections/CartHeader.js
@@ -5,7 +5,8 @@ import * as styles from "./CartHeader.module.css";
 const CartHeader = ({ closeCart }) => {
   return (
     <div className={styles.cartHeader}>
-      <span>Shopping Cart</span>
+      <div></div>
+      <h1><span>Shopping Bag</span></h1>
       <button className={styles.closeCartBtn} onClick={closeCart}>
         Close
       </button>
