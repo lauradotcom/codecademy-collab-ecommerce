3e4a8a86046dc60b744e1f7d9201ed96938d2e33
Chris DeCleene <chris_decleene@ymail.com>
2022-01-21 20:55:39 -0700

    Mode ProductImageSlider component to ProductPage sections folder.

---
+++ src/page-components/ProductPage/Sections/ProductImageSlider.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import * as styles from "./ProductImageSlider.module.css";
+
+const ProductImageSlider = ({ product }) => {
+  const [imageIndex, setImageIndex] = useState(0);
+  const imageCount = product.images.length;
+
+  const decrement = () => {
+    if (imageIndex > 0) {
+      setImageIndex((prevState) => prevState - 1);
+    }
+  };
+
+  const increment = () => {
+    if (imageIndex < imageCount - 1) {
+      setImageIndex((prevState) => prevState + 1);
+    }
+  };
+
+    const imageChangeHandler = (index) => {
+      setImageIndex(index)
+    };
+
+  return (
+    <>
+      <div className={styles.slider}>
+        <button onClick={decrement}>{"<"}</button>
+        <GatsbyImage
+          image={getImage(product.images[imageIndex])}
+          alt={product.title}
+        />
+        <button onClick={increment}>{">"}</button>
+      </div>
+      <div className={styles.gallery}>
+        {product.images.map((image, index) => (
+          <GatsbyImage
+            image={getImage(image)}
+            alt={product.title + " #" + index}
+            onClick={() => imageChangeHandler(index)}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ProductImageSlider;
