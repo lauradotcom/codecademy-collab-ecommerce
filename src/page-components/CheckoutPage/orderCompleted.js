be19c2c8b7f7e5814b4ed931c8ea7fcd9f889ef6
Chris DeCleene <chris_decleene@ymail.com>
2022-01-24 14:33:04 -0700

    Remove unused style that was throwing error in console.

--- src/page-components/CheckoutPage/orderCompleted.js
+++ src/page-components/CheckoutPage/orderCompleted.js
@@ -9,7 +9,6 @@ const OrderCompleted = ({ firstName }) => {
       <img 
         src='https://cdn-icons-png.flaticon.com/512/1008/1008010.png' 
         alt='order completed' 
-        className={styles.orderCompleted}
       />
       <p>{`Thank you for your order${firstName ? ` ${firstName}` : ''}!`}</p>
       <p>We'll be reviewing your order and send you an email when it's ready.</p>
