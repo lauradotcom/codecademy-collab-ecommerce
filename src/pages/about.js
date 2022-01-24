31e9b92485ca47e5b233040eafef978772952808
Chris DeCleene <chris_decleene@ymail.com>
2022-01-23 20:44:14 -0700

    Create about page with GraphQL query for featured products with AboutPage component and styling to display dummy data.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import AboutPage from '../page-components/AboutPage';
+import { graphql } from "gatsby";
+import React from "react";
+import AboutPage from "../page-components/AboutPage";
 
-const About = () => {
-  return <AboutPage />;
+const About = ({ data }) => {
+  const products = data.allProductsJson.nodes;
+  return <AboutPage products={products} />;
 };
+
+export const query = graphql`
+  {
+    allProductsJson {
+      nodes {
+        discount
+        name
+        price
+        productId
+        id
+        images {
+          childImageSharp {
+            gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default About;
