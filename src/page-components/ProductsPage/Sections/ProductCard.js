83abcd96e4199a9556bd0b2013c259462c5e61cb
Chris DeCleene <chris_decleene@ymail.com>
2022-01-23 18:57:37 -0700

    Separate mapped products into product card component

---
+++ src/page-components/ProductsPage/Sections/ProductCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import ProductPrice from "./ProductPrice";
+import * as styles from "../ProductsPage.module.css";
+
+const ProductCard = ({ product }) => {
+  return (
+    <section key={product.id} className={styles.section}>
+      <Link to={`/products/${product.productId}`}>
+        <GatsbyImage
+          className={styles.image}
+          image={getImage(product.images[0])}
+          alt={product.name}
+        />
+      </Link>
+      <div className={styles.detailsActions}>
+        <Link to={`/products/${product.productId}`}>
+          <h2>{product.name}</h2>
+        </Link>
+        <ProductPrice product={product} />
+      </div>
+    </section>
+  );
+};
+
+export default ProductCard;
--- src/page-components/ProductsPage/index.js
+++ src/page-components/ProductsPage/index.js
@@ -1,11 +1,6 @@
 import * as React from "react";
-import { Link } from "gatsby";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
-// components
-// page sections
-import ProductPrice from "./Sections/ProductPrice";
-// styles
 import * as styles from "./ProductsPage.module.css";
+import ProductCard from "./Sections/ProductCard";
 
 const ProductsPage = ({ products }) => {
   return (
@@ -15,21 +10,7 @@ const ProductsPage = ({ products }) => {
         <h1>Products Page</h1>
         <section className={styles.main}>
           {products.map((product) => (
-            <section key={product.id} className={styles.section}>
-              <Link to={`/products/${product.productId}`}>
-                <GatsbyImage
-                  className={styles.image}
-                  image={getImage(product.images[0])}
-                  alt={product.name}
-                />
-              </Link>
-              <div className={styles.detailsActions}>
-                <Link to={`/products/${product.productId}`}>
-                  <h2>{product.name}</h2>
-                </Link>
-                <ProductPrice product={product} />
-              </div>
-            </section>
+            <ProductCard key={product.id} product={product} />
           ))}
         </section>
       </main>
